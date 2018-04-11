2a0b387ba0149a3397e802ce22ff6d1dd73a2ea0
Maciek Bulski <fragmast3r@gmail.com>
2018-04-11 09:00:54 +0100

    fixed errors in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-var x = 5;
-function drawTree(x) {
-  for (var i = 1; i <= x; i++) {
-    var star = "";
-    for (var j = 1; j <= i; j++) {
-      var star = (star += "*");
+function drawTree(height) {
+    for (var i = 1; i <= height; i++) {
+        var star = "";
+        for (var j = 1; j <= i; j++) {
+            star += "*";
+        }
+        console.log(star);
     }
-    console.log(star);
-  }
 }
 drawTree(5);
